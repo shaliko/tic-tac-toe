c4d03f95a08024218839fae3e256b02e10d349ea
Shaliko Usubov <shaliko@ezid.ru>
2023-09-19 18:13:08 +0300

    Run linter

--- game_api/config/initializers/cors.rb
+++ game_api/config/initializers/cors.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # Be sure to restart your server when you modify this file.
 
 # Avoid CORS issues when API is called from the frontend app.
@@ -10,8 +11,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins ENV['APP_HOST']
 
-    resource "*",
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head] # TODO: remove not used methods
+    resource '*',
+             headers: :any,
+             methods: %i[get post put patch delete options head] # TODO: remove not used methods
   end
 end
