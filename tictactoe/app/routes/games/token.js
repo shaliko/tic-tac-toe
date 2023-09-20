44a88825fcf5684375be096133146799b76a81e1
Shaliko Usubov <shaliko@ezid.ru>
2023-09-20 11:31:53 +0300

    Add polling

--- tictactoe/app/routes/games/token.js
+++ tictactoe/app/routes/games/token.js
@@ -4,13 +4,25 @@ import { service } from '@ember/service';
 export default class GamesTokenRoute extends Route {
   @service store;
 
+  timer;
+
+  activate() {
+    this.timer = setInterval(() => {
+      const { game_id } = this.paramsFor(this.routeName);
+
+      this.store.findRecord('game', game_id, { reload: true });
+    }, 2000);
+  }
+
+  deactivate() {
+    clearInterval(this.timer);
+  }
+
   async model(params) {
     let gameId = params.game_id;
     let token = params.token;
 
     let game = await this.store.findRecord('game', gameId);
-
-    console.log(game);
 
     return { game, token };
   }
