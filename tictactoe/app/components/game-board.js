8f17a15cf918c7ff60c555d1f670100b0e55cc9c
Shaliko Usubov <shaliko@ezid.ru>
2023-09-20 12:13:40 +0300

    Polish GameBoard logic

--- tictactoe/app/components/game-board.js
+++ tictactoe/app/components/game-board.js
@@ -2,6 +2,18 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 
 export default class GameBoardComponent extends Component {
+  get isMyTurn() {
+    const { game, currentPlayerToken } = this.args;
+
+    const currentUserSymbol = game.player1_token === currentPlayerToken ? game.player1_symbol : game.player2_symbol;
+
+    return currentUserSymbol === game.current_symbol;
+  }
+
+  get board() {
+    return this.args.game.state;
+  }
+
   @action
   handleMove(rowIndex, columnIndex) {
     if (this.args.onMove) {
